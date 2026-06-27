3fb51c002b5423ee6f486888191b00dd97df29c9
SarthakChaudhari-2905 <chaudharisarthak295@gmail.com>
2026-06-28 01:05:51 +0530

    Add profile annotation to RedisTestController

--- src/main/java/com/Ecommerce/demo/test/RedisTestController.java
+++ src/main/java/com/Ecommerce/demo/test/RedisTestController.java
@@ -2,6 +2,7 @@ package com.Ecommerce.demo.test;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
